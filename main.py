1143f9935767a35897566f99e2b0884c7ed25bb8
chilczukg <chilczukg@gmail.com>
2017-06-17 00:39:34 +0200

    GREEN: task2 is passing tests

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from task1 import group_by
-from task2 import damage
+from task2 import *
 
 file = open('launchlog.txt')
-print(group_by(file, 'month', success=False)) # in the instruction out wasn't filtered, that's why outs are diffrent
+# print(group_by(file, 'month', success=False)) # in the instruction out wasn't filtered, that's why outs are diffrent
 
+print('fejejeeaindaiyaiai')
+print(damage('xxxxxfejejeeaindaiyaiaixxxxxx'), '\n')
+#
+# print(split_smallest_subspells('jejeeaindaiyai'))
+# print(split_biggest_subspells('jejeeaindaiyai'))
 
-print(damage('xxxxxfejejeeaindaiyaiaixxxxxx'))
+print('\ntesty:')
+print(damage('feeai') == 2)
+print(damage('feaineain') ==  7)
+print(damage('jee') == 0)
+print(damage('fefefefefeaiaiaiaiai') == 0)
+print(damage('fdafafeajain') == 1)
+print(damage('fexxxxxxxxxxai') == 0)
+
+# print('\n\n')
+# print(damage('feaineain') ==  7)
+# print(damage('feaineain'))
+# print()
--- task2.py
+++ task2.py
@@ -6,6 +6,7 @@ SUBSPELLS = {'fe' : 1,
              'ne' : 2,
              'ai' : 2,
              '-' : -1}
+POSSIBLE_PAIRS = {'fe', 'je', 'ee', 'ai', 'in', 'da', 'ne'}
 
 
 def damage(spell):
@@ -13,16 +14,94 @@ def damage(spell):
     end = spell.rfind('ai') + 2
     spell = spell[start:end]
 
-    spell_is_correct = start >=0 and end >= 0
-    founded_subspells = []
+    spell_is_correct = 0 <= start < end and len(spell) > 0
 
-    print(spell)
-    curr_subspell = spell[0]
-    for i in range(1,len(spell)):
-        pass
+    if spell_is_correct:
+        # print('spell na początku:',spell)
+        founded_subspells = [spell[:2], spell[-2:]]
+        spell = spell[2:-2]
+
+        if spell.find('fe') > -1:
+            spell_is_correct = False
+        else:
+            creation = ''
+            # print('spell teraz:', spell)
+            while len(spell) > 2:
+                # print("mamy:", spell)
+                creation += spell[0]
+                spell=spell[1:]
+                maybe = creation[-1]+spell[0]
+                # print('               maybe:', maybe, maybe not in POSSIBLE_PAIRS)
+                if maybe not in POSSIBLE_PAIRS:
+                    # print(maybe, 'gdy not in pairs')
+                    if len(creation) > 2:
+                        founded_subspells += split_max_damage_subspells(creation)
+                    else:
+                        founded_subspells += split_smallest_subspells(creation)
+                    creation = ''
+            if len(creation) > 2:
+                founded_subspells += split_max_damage_subspells(creation)
+            else:
+                founded_subspells += split_smallest_subspells(creation)
+            founded_subspells.append(spell)
+            # print(founded_subspells)
+            # print(spell)
+
+        evaluated_damage = evaluate_subspells(founded_subspells)
+        return evaluated_damage if spell_is_correct and evaluated_damage > 0 else 0
+
+    return 0
 
 
 def evaluate_subspells(list_of_subspells):
-    return sum(map(lambda sub: SUBSPELLS[sub], list_of_subspells))
+    return sum(map(lambda sub: SUBSPELLS.get(sub, -1*len(sub)), list_of_subspells))
+
+def split_max_damage_subspells(string):
+    small = split_smallest_subspells(string)
+    big = split_biggest_subspells(string)
+
+    return small if evaluate_subspells(small) > evaluate_subspells(big) else big
+
+def split_smallest_subspells(string):
+    splited = []
+    while len(string) > 2:
+        working = string[:2]
+        if working in SUBSPELLS:
+            splited.append(working)
+            string = string[2:]
+        else:
+            working += string[2]
+            if working in SUBSPELLS:
+                splited.append(working)
+                string = string[3:]
+            else:
+                splited.append(working[0])
+                string = string[1:]
+
+    splited.append(string)
+    return splited
+
+
+def split_biggest_subspells(string):
+    splited = []
+    while len(string) > 3:
+        working = string[:3]
+        if working in SUBSPELLS:
+            splited.append(working)
+            string = string[3:]
+        else:
+            working = working[:2]
+            if working in SUBSPELLS:
+                splited.append(working)
+                string = string[2:]
+            else:
+                splited.append(working[0])
+                string = string[1:]
+    if string in SUBSPELLS:
+        splited.append(string)
+    else:
+        splited += split_smallest_subspells(string)
+    return splited
+
 
 
